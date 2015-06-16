b2e2f9a8a331fee292a58eca19e1ba2c49b75655
Neilson Ramalho <neilsoncarlos@gmail.com>
2015-06-16 21:13:06 +0200

    Initial commit - setting up the project

---
+++ src/main/java/Test.java
@@ -0,0 +1,8 @@
+/**
+ * Created by neilson on 6/16/15.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Sind Sie auf der Gästeliste?!");
+    }
+}
